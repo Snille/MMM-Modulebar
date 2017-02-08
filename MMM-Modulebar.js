ee3367a42f78e383b051aa1f609fcd8f08c80262
Erik Pettersson <mrsnille@gmail.com>
2017-02-08 14:23:19 +0100

    Removed support for Weather Icons, to fix size functionallity and documented the code a bit.

--- MMM-Modulebar.js
+++ MMM-Modulebar.js
@@ -1,10 +1,10 @@
 /* global Module */
 
-/* Magic Mirror
+/* Magic Mirror 2
  * Module: MMM-Modulebar
  *
- * By by Erik Pettersson
- * Based on the TouchNavigation Module from Brian Janssen
+ * By Erik Pettersson
+ * Based on the TouchNavigation Module by Brian Janssen
  *
  * MIT Licensed.
  */
@@ -27,15 +27,15 @@ Module.register("MMM-Modulebar",{
             "clock": {
 				// The text to be displayed in the button.
 				text:	"Clock",
-				// Then symbol from either font-awesome or weather-icons. OBS: Full icon class name!
-                symbol: "fa fa-clock-o",
+				// Then symbol from font-awesome!
+                symbol: "clock-o",
             }
 		}
     },
 
     // Define required styles.
 	getStyles: function(){
-		return ["weather-icons.css", "font-awesome.css", "MMM-Modulebar.css"];
+		return ["font-awesome.css", "MMM-Modulebar.css"];
 	},
 	
     // Override dom generator.
@@ -51,75 +51,85 @@ Module.register("MMM-Modulebar",{
 
         return menu;
     },
-
+	// Creates the buttons.
     createButton: function (self, name, data, placement) {
-        var item = document.createElement("span");
+		// Creates the span elemet to contain all the buttons.
+		var item = document.createElement("span");
         item.id = self.identifier + "_button_" + name;
         item.className = "modulebar-button";
         item.style.minWidth = self.config.minWidth;
         item.style.minHeight = self.config.minHeight;
-        
+		// Collects all modules loaded in MagicMirror.
 		var modules = MM.getModules();
-           item.addEventListener("click", function () {
+			// When a button is clicked, the module either gets hidden or shown depending on current module status.
+			item.addEventListener("click", function () {
 			for (var i = 0; i < modules.length; i++) {
 				if (modules[i].name === name) {
-					console.log(modules[i].name);
 					if (modules[i].hidden) {
 						modules[i].show(self.config.animationSpeed);
+						console.log("Showing "+modules[i].name);
 					}else{
 						modules[i].hide(self.config.animationSpeed);
+						console.log("Hiding "+modules[i].name);
 					}
 				}
 			}
 		});
-
+		// Fixes the aligning.
         item.style.flexDirection = {
             "right"  : "row-reverse",
             "left"   : "row",
             "top"    : "column",
             "bottom" : "column-reverse"
         }[placement];
-
+		// Sets the border around the symbol/picture/text to black.
         if (!self.config.showBorder) {
             item.style.borderColor = "black";
         }
-
+		// Adds the Font-Awesome symbol if specified.
         if (data.symbol) {
             var symbol = document.createElement("span");
-            symbol.className = "modulebar-picture " + data.symbol;
-
+            symbol.className = "modulebar-picture fa fa-" + data.symbol;
+			// Sets the size on the symbol if specified.
+            if (data.size) {
+                symbol.className += " fa-" + data.size;
+                symbol.className += data.size == 1 ? "g" : "x";
+            }
+			// Align the symbol with a margin.
             if (data.text && placement === "left") {
                 symbol.style.marginRight = "4px";
             }
-
+			// Adds the symbol to the item.
             item.appendChild(symbol);
-        } else if (data.img) {
+
+		// Adds a picture if specified.
+		} else if (data.img) {
             var image = document.createElement("img");
             image.className = "modulebar-picture";
             image.src = data.img;
-
+			// Sets the size of the picture if specified.
             if (data.width)  image.width  = data.width;
             if (data.height) image.height = data.height;
-            
+			// Align the picture with a margin.
             if (data.text && placement === "left") {
                 image.style.marginRight = "4px";
             }
-
+			// Adds the picture to the item.
             item.appendChild(image);
         }
-
+		// Adds the text if specified.
         if (data.text) {
             var text = document.createElement("span");
             text.className = "modulebar-text";
             text.innerHTML = data.text;
-
+			// Align the text with a margin.
             if ((data.symbol || data.img) && placement === "right") {
                 text.style.marginRight = "4px";
             }
-
+			// Adds the text to the item.
             item.appendChild(text);
         }
-
+		// All done. :)
         return item;
     }
 });	
